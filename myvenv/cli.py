5e2ec1ac6326e1ae788a4e3ba0482f1b8882d757
Ricky Teachey <ricky@teachey.org>
2019-11-14 00:32:57 -0500

    Fixed multiple install arguments problem

--- myvenv/cli.py
+++ myvenv/cli.py
@@ -3,22 +3,30 @@ import click
 import subprocess
 
 
-@click.command()
-@click.argument("path", type=click.Path())
-@click.option("--install", "-i", "install_list", multiple=True)
-def main(path, install_list):
+@click.group()
+@click.argument("path", type=click.Path(exists=False, file_okay=False, allow_dash=False))
+@click.pass_context
+def main(ctx, path):
     path = pathlib.Path(path)
+    ctx.obj = dict(path = path)
     subprocess.run(["mkdir", str(path)], check=True)
     subprocess.run(["virtualenv", str(path/"venv")], check=True)
+
+
+@main.command()
+@click.argument("install", nargs=-1)
+@click.pass_context
+def install(ctx, install):
+    path = ctx.obj['path']
     windows = isinstance(path, pathlib.WindowsPath)
     if windows:
-        if install_list:
-            subprocess.run([str(path/"venv/Scripts/python"), "-m", "pip", "install", *install_list], check=True)
-        # TODO: this does not activate the virtual environement as expected in Windows
+        if install:
+            subprocess.run([str(path/"venv/Scripts/python"), "-m", "pip", "install", *install], check=True)
+        # TODO: this does not activate the virtual environment as expected in Windows
         subprocess.run([str(path / r"venv/Scripts/activate.bat")], check=True)
     else:
         # assume posix
-        if install_list:
-            subprocess.run([str(path/"venv/bin/python"), "-m", "pip", "install", *install_list], check=True)
-        # TODO: this does not activate the virtual environement as expected in Posix
+        if install:
+            subprocess.run([str(path/"venv/bin/python"), "-m", "pip", "install", *install], check=True)
+        # TODO: this does not activate the virtual environment as expected in Posix
         subprocess.run(["source", str(pathlib.Path(r"venv\bin\activate"))], check=True)
--- test_cli.py
+++ test_cli.py
@@ -1,18 +1,16 @@
-import subprocess
-
 import pytest
 from click.testing import CliRunner
 from myvenv.cli import main
 
 
 @pytest.mark.parametrize('path, install', [
-    ('path', []),
-    ('path', ['click', 'pent']),
+    ('venv', []),
+    ('venv', ['click', 'pent']),
 ])
 def test_main(tmp_path, path, install):
     runner = CliRunner()
     if install:
-        result = runner.invoke(main, [str(tmp_path/path), '-i', *install])
+        result = runner.invoke(main, [str(tmp_path/path), 'install', *install], catch_exceptions=False)
     else:
-        result = runner.invoke(main, [str(tmp_path/path)])
+        result = runner.invoke(main, [str(tmp_path/path)], catch_exceptions=False)
     assert result.exit_code == 0
